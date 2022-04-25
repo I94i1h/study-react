ae5ecc1b9b17bfe5f81d00e18448fb8df4c56011
DanhNX <I94i1h@gmail.com>
2022-04-25 11:18:58 +0700

    add folder structures

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import ColorBox from './components/ColorBox'
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const colorlist = ['red', 'blue', 'green']
+
   return (
     <div className="App">
-      <section>
-        <ColorBox color="red"> </ColorBox>
-        <ColorBox color="blue"> </ColorBox>
-        <ColorBox color="yellow"> </ColorBox>
-      </section>
+      <header className='App-header'>
+        <img src={logo} className="App-logo" alt="logo" />
+        <ul>
+          {colorlist.map(color => (
+            <li key={color} style={{ color }}> {color} </li>
+          ))}
+        </ul>
+      </header>
     </div>
   );
 }
